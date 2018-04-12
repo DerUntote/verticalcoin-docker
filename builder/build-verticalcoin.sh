bdc9090967a6f8f7dcad2170ad3f9102816cee70
Lukas-Christian Schulze <lukas.schulze@gmail.com>
2018-04-12 23:57:45 +0200

    rename

--- builder/build-verticalcoin.sh
+++ builder/build-verticalcoin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ! -d /verticalcoin ]]; then
-    cd / && git clone --depth 1 -b vnode https://github.com/DerUntote/verticalcoin-docker /verticalcoin
+    cd / && git clone --depth 1 -b vnode https://github.com/DerUntote/verticalcoin /verticalcoin
 fi
 
 cd /verticalcoin && git checkout vnode && git pull origin vnode && /verticalcoin/autogen.sh && ./configure && make
