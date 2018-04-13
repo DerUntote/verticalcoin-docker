c448868169f05f137ac12bb7bbba447b17f93724
Lukas-Christian Schulze <lukas.schulze@gmail.com>
2018-04-13 08:58:09 +0200

    fixed: Use master branch

--- builder/build-verticalcoin.sh
+++ builder/build-verticalcoin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ! -d /verticalcoin ]]; then
-    cd / && git clone --depth 1 -b vnode https://github.com/DerUntote/verticalcoin /verticalcoin
+    cd / && git clone --depth 1 -b master https://github.com/DerUntote/verticalcoin /verticalcoin
 fi
 
-cd /verticalcoin && git checkout vnode && git pull origin vnode && /verticalcoin/autogen.sh && ./configure && make
+cd /verticalcoin && git checkout master && git pull origin master && /verticalcoin/autogen.sh && ./configure && make
