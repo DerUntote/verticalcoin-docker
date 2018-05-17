e3b478b6d910ed236b3bf706c259d6c7c559ce1e
Lukas-Christian Schulze <lukas.schulze@gmail.com>
2018-05-17 19:48:42 +0200

    ADDED VNodes Support

--- vNode/config-vNode.sh
+++ vNode/config-vNode.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+echo "we will now create a simple vNode"
 CONFIG = "${HOME}/.verticalcoin/verticalcoin.conf"
+BINDIR = "${HOME}/verticalcoin-bin"
+
 
 if [[ -f $CONFIG ]]; then
     while true; do
@@ -16,11 +19,11 @@ if [[ ! -d ${HOME}/.verticalcoin ]]; then
     mkdir -p ${HOME}/.verticalcoin
 fi
 
-if [[ ! -d ${HOME}/verticalcoin-bin ]] || [[ ! -f $CONFIG ]]; then
+if [[ ! -d $BINDIR ]] || [[ ! -f $CONFIG ]]; then
     RPCPW=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
     
-    mkdir    -p ${HOME}/verticalcoin-bin
-    cp -f bin/* ${HOME}/verticalcoin-bin
+    mkdir    -p $BINDIR
+    cp -f bin/* $BINDIR
     cat <<EOM >$CONFIG
 conf=$CONFIG
 
@@ -42,7 +45,14 @@ addnode=H08.vrtnode.ovh
 addnode=H09.vrtnode.ovh
 addnode=H10.vrtnode.ovh
 
+daemon=1
+vnode=1
+vnodeprivkey=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  ## Replace with your vnode private key
+externalip=xxx.xxx.xxx.xxx:54111 ## Replace with your node external IP
 
 EOM
 
 fi
+
+
+echo "edit vnodeprivkey in $CONFIG"
